c4074ed0d4dd2ec23f3d76730884b26941aaecf7
Pastori <pastoriaraar69@gmail.com>
2024-02-28 19:00:31 +0100

    Containerize Project

--- Server/app.py
+++ Server/app.py
@@ -64,4 +64,4 @@ with app.app_context():
     #db.drop_all()
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=8080, debug=True)
+    socketio.run(app, host="0.0.0.0", port=8080, debug=True, allow_unsafe_werkzeug=True)
